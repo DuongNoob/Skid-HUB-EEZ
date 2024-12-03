99115cb8b1db0f86e27cfe9798162be280bc04c5
DuongNoob <96777924+DuongNoob@users.noreply.github.com>
2024-12-03 19:31:30 +0700

    Update SkidHub_Xd_Part2.lua

--- SkidHub_Xd_Part2.lua
+++ SkidHub_Xd_Part2.lua
@@ -281,25 +281,6 @@ if getgenv().fpsBoost then
                 v:Destroy()
             end
         end
-       for i,v in next, workspace:GetDescendants() do
-    pcall(function()
-        v.Transparency = 1
-    end)
-end
-for i,v in next, getnilinstances() do
-    pcall(function()
-        v.Transparency = 1
-        for i1,v1 in next, v:GetDescendants() do
-            v1.Transparency = 1
-        end
-    end)
-end
-a = workspace
-a.DescendantAdded:Connect(function(v)
-    pcall(function()
-        v.Transparency = 1
-    end)
-end) 
 end
 hookfunction(workspace.Raycast, function()
     return
