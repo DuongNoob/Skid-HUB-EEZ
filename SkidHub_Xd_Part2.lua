63e45b40593a40755cb5c54cd13b09cadef1ee73
DuongNoob <96777924+DuongNoob@users.noreply.github.com>
2025-10-22 21:56:41 +0700

    Update SkidHub_Xd_Part2.lua

--- SkidHub_Xd_Part2.lua
+++ SkidHub_Xd_Part2.lua
@@ -40,6 +40,42 @@ repeat task.wait() until game:IsLoaded() and game.Players.LocalPlayer and game.P
 
 
 local LocalPlayer = game.Players.LocalPlayer
+local Character = LocalPlayer.Character
+repeat task.wait() until Character:FindFirstChild("RemoteEvent") and Character:FindFirstChild("RemoteFunction")
+local RemoteFunction, RemoteEvent = Character.RemoteFunction, Character.RemoteEvent
+local HRP = Character.PrimaryPart
+local part
+local dontTPOnDeath = true
+
+
+if not LocalPlayer.PlayerGui:FindFirstChild("HUD") then
+    print("I FOUND IT")
+    local HUD = game:GetService("ReplicatedStorage").Objects.HUD:Clone()
+    HUD.Parent = LocalPlayer.PlayerGui
+end
+
+print("I DID FOUND IT, MAYBE IT WILL WORK?")
+RemoteEvent:FireServer("PressedPlay")
+
+if LocalPlayer.PlayerGui:FindFirstChild("LoadingScreen1") then
+ wait(2)
+    if LocalPlayer.PlayerGui:FindFirstChild("LoadingScreen1") then
+    LocalPlayer.PlayerGui:FindFirstChild("LoadingScreen1"):Destroy()
+    end
+end
+
+if LocalPlayer.PlayerGui:FindFirstChild("LoadingScreen") then
+    LocalPlayer.PlayerGui:FindFirstChild("LoadingScreen"):Destroy()
+end
+
+task.spawn(function()
+    if game.Lighting:WaitForChild("DepthOfField", 10) then
+        game.Lighting.DepthOfField:Destroy()
+    end
+end)
+
+workspace.Map.IMPORTANT.OceanFloor.OceanFloor_Sand_6.Size = Vector3.new(2048, 89, 2048)
+workspace.Map.IMPORTANT.OceanFloor.OceanFloor_Sand_4.Size = Vector3.new(2048, 89, 2048)
 
 task.spawn(function()
     local IAMGOINGINSANEFROMYOU = true
@@ -199,40 +235,6 @@ end
 
 serverHop()
 
-if not LocalPlayer.PlayerGui:FindFirstChild("HUD") then
-    local HUD = game:GetService("ReplicatedStorage").Objects.HUD:Clone()
-    HUD.Parent = LocalPlayer.PlayerGui
-end
-
-print("I DID FOUND IT, MAYBE IT WILL WORK?")
-RemoteEvent:FireServer("PressedPlay")
-
-if LocalPlayer.PlayerGui:FindFirstChild("LoadingScreen1") then
- wait(2)
-    if LocalPlayer.PlayerGui:FindFirstChild("LoadingScreen1") then
-    LocalPlayer.PlayerGui:FindFirstChild("LoadingScreen1"):Destroy()
-    end
-end
-
-if LocalPlayer.PlayerGui:FindFirstChild("LoadingScreen") then
-    LocalPlayer.PlayerGui:FindFirstChild("LoadingScreen"):Destroy()
-end
-
-pcall(function()
-    for _,v in pairs(getconnections(LocalPlayer.Idled)) do
-        v:Disable()
-    end
-
-    print("Xenon On TOP! Made with love by dank, Xenon Invite Link:https://discord.gg/YrAa5ngDPv")
-    LocalPlayer.Character.RemoteEvent:FireServer("PressedPlay")
-
-    LocalPlayer.PlayerGui:FindFirstChild("LoadingScreen1"):Destroy()
-    task.wait(1)
-    LocalPlayer.PlayerGui:FindFirstChild("LoadingScreen"):Destroy()
-    
-    workspace.Map.IMPORTANT.OceanFloor.OceanFloor_Sand_6.Size = Vector3.new(2048, 89, 2048)
-    workspace.Map.IMPORTANT.OceanFloor.OceanFloor_Sand_4.Size = Vector3.new(2048, 89, 2048)
-end)
 local function FireButton(x)
     for i, v in pairs(getconnections(x.MouseButton1Click)) do 
         v:Fire() wait(0.5)
